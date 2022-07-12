5751bb08e282e0b3be8ae4f7a6af8812e1d8f024
Oby <obyli214@gmail.com>
2022-07-12 01:15:31 -0700

    completed lab 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Nft(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    collection_name = models.CharField(max_length=100, null=True)
+    price = models.IntegerField(null=True)
+    description = models.TextField(max_length=250, null=True)
+
+    def __str__(self):
+        return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('nfts/', views.nfts_index, name='index')
+    path('nfts/', views.nfts_index, name='index'),
+    path('nfts/<int:nft_id>/', views.nfts_detail, name='detail'),
 ]
